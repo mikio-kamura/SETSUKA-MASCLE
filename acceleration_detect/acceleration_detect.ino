ef30d1bd7243b82995aca204505d3058fd0230fc
mikio <50087797+mikio-kamura@users.noreply.github.com>
2022-01-23 15:05:30 +0900

    arduinoで加速度取得

---
+++ acceleration_detect/acceleration_detect.ino
@@ -0,0 +1,24 @@
+//********************************
+//加速度センサの値を取得するプログラム
+//********************************
+void setup()
+{
+// シリアルモニターの初期化をする
+Serial.begin(9600) ;
+}
+void loop()
+{
+long x , y , z ;
+x = y = z = 0 ;
+x = analogRead(3) ; // Ｘ軸
+y = analogRead(4) ; // Ｙ軸
+z = analogRead(5) ; // Ｚ軸
+Serial.print("X:") ;
+Serial.print(x) ;
+Serial.print(" Y:") ;
+Serial.print(y) ;
+Serial.print(" Z:") ;
+Serial.println(z) ;
+delay(50) ;
+exit(0);
+}
