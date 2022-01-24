ffc0479a515548aa72e6b76b315c3c8b47cd2e91
mikio <50087797+mikio-kamura@users.noreply.github.com>
2022-01-24 21:24:51 +0900

    small change

--- stopwatch.js
+++ stopwatch.js
@@ -1,6 +1,5 @@
 let watch_start = document.querySelector(".btn-start");
-
 watch_start.addEventListener("click", () =>{
     window.location.href = './watch_start.html';
-    window.open('watch_start.html')
+    // window.open('watch_start.html')
 })
--- stopwatch.php
+++ stopwatch.php
@@ -2,20 +2,28 @@
 echo <<<EOM
 <script type="text/javascript">
 alert( "TEST" )
+console.log('.json_encode($data).');
 </script>
 EOM;
+$link = mysqli_connect('127.0.0.1', 'mikio', 'pass', 'stepping_record');
+if (mysqli_connect_errno()){
+    die("データベースに接続不可:" . mysqli_connect_error() . "\n");
+}else{
+    echo "データベースの接続に成功しました。\n";
+}
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>javascriptとphpの連携</title>
+        <title>stopwatch_home</title>
         <link rel="stylesheet" href="style/style.css">
     </head>
     <body>
-    <div class="scss-test-container">scss test</div>
-    <div class="btn btn-gradient btn-start"><span>START!</span></div>
-    <form acttion="stopwatch.php" method="post">
+        <div class="scss-test-container">SETSUKA-WATCHを始めましょう</div>
+        <div class="btn btn-gradient btn-start"><span>START!</span></div>
+        <form acttion="stopwatch.php" method="post">
+        <script src="stopwatch.js"></script>
     </body>
 </html>
